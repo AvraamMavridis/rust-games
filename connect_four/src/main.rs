f339e96c1eb478532db53c99cfc5b8ca5bb13a6d
AvraamMavridis <avraammauridis@gmail.com>
2024-05-12 07:58:50 +0300

    feat: get user prompt and assign column

--- connect_four/src/main.rs
+++ connect_four/src/main.rs
@@ -28,6 +28,14 @@ fn main() {
             _ => continue, // Re-prompt on invalid input
         };
 
+        if let Some(free_row) = find_open_slot_in_row(&board, col){
+            board[col][free_row] = current_player;
+        } else {
+            println!("Column full, try a different one.");
+        }
+
+        println!("{}", col)
+
     }
 }
 
@@ -39,4 +47,15 @@ fn print_board(board: &Board) {
         println!();
     }
     println!();
+}
+
+
+fn find_open_slot_in_row(board: &Board, col: usize) -> Option<usize> {
+    for row in (1..=6).rev() {
+        println!(">>>>>>>>>{}", row);
+        if(board[col][row] == ' '){
+            return Some(row);
+        }
+    }
+    return None;
 }
